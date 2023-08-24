b5af6379ec2696f11d6ded866d597fa6afbb4913
raja9542 <rajasekhar1banda@gmail.com>
2023-08-24 13:06:24 +0530

    frontend

--- special-vars.sh
+++ special-vars.sh
@@ -1,8 +1,30 @@
+echo "following values from script"
+
 echo "0 - $0"
 echo "1 - $1"
 echo "2 - $2"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+ # To see output execute below command
+#/* sudo bash special-vars.sh abc 10 20
+#output
+#0 - special-vars.sh
+#1 - abc
+#2 - 10
+#* - abc 10 20
+#@ - abc 10 20
+# # - 3
 
+example() {
+echo "following values from function"
+echo "0 - $0"
+echo "1 - $1"
+echo "2 - $2"
+echo "* - $*"
+echo "@ - $@"
+echo "# - $#"
+}
+
+example
 
