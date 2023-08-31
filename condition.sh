4f5def3b796899af6dbaa3a4c69ea42fb793d0fb
raja9542 <rajasekhar1banda@gmail.com>
2023-08-31 20:27:25 +0530

    frontend

---
+++ condition.sh
@@ -0,0 +1,21 @@
+read -p 'Enter Your Course Name:' course_name
+
+case ${course_name} in
+
+AWS)
+  echo "Welcome to AWS Training"
+  ;;
+Azure)
+  echo "Welcome to Azure Training"
+  ;;
+*)
+  echo "unknown Course Name"
+;;
+esac
+
+if [ "${course_name}" == "AWS" ]; then
+    echo "Welcome to AWS Training"
+elif [ "${course_name}" == "Azure" ]; then
+else
+    echo "unknown Course Name"
+fi
